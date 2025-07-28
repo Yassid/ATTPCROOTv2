796d89365b8e42faf78e181a34748f14b82ab740
Adam Anthony <aanthon2@highpoint.edu>
2025-07-29 00:33:41 +0200

    Cleanup UKF doc and headers

--- AtReconstruction/AtFitter/OpenKF/kalman_filter/TrackFitterUKF.h
+++ AtReconstruction/AtFitter/OpenKF/kalman_filter/TrackFitterUKF.h
@@ -26,49 +26,54 @@
 #include "unscented_kalman_filter.h"
 namespace kf {
 
-/// @brief Class for fitting tracks using the Unscented Kalman Filter (UKF) algorithm.
-/// @tparam DIM_X Dimension of the state vector.
-/// @tparam DIM_Z Dimension of the measurement vector.
-/// @tparam DIM_V Dimension of the process noise vector.
-/// @tparam DIM_N Dimension of the measurement noise vector.
+/**  @brief Class for fitting tracks using the Unscented Kalman Filter (UKF) algorithm.
+ *
+ * Serves as a templated base class for UKF calculations. It hold no physics information, just
+ * the machinery that underlies the UKF formalism. It is a modified version of the UKF provided
+ * by OpenKF, that has been expanded to allow for more hooks into the method.
+ *
+ * Templated because I believe Eigen can do quite a bit of operation for small matrices like we have here if
+ * the size is known at compile time. Worth checking that though.
+ *
+ * @tparam DIM_X Dimension of the state vector.
+ * @tparam DIM_Z Dimension of the measurement vector.
+ * @tparam DIM_V Dimension of the process noise vector.
+ * @tparam DIM_N Dimension of the measurement noise vector.
+ */
 template <int32_t DIM_X = 6, int32_t DIM_Z = 3, int32_t DIM_V = 2, int32_t DIM_N = 3>
-class TrackFitterUKFBase : public KalmanFilter<DIM_X, DIM_Z> {
-
+class TrackFitterUKFBase {
 public:
    // Augmented state vector is just the process noise and state vector. The measurement noise is not included as that
    // is independent of the propagation and measurement model and just adds linearly.
    static constexpr int32_t DIM_A{DIM_X + DIM_V};       ///< @brief Augmented state dimension
    static constexpr int32_t SIGMA_DIM_A{2 * DIM_A + 1}; ///< @brief Sigma points dimension for augmented state
-
-protected:
-   using KalmanFilter<DIM_X, DIM_Z>::m_vecX; // from Base KalmanFilter class
-   using KalmanFilter<DIM_X, DIM_Z>::m_matP; // from Base KalmanFilter class
-
-   float32_t m_weight0;  /// @brief unscented transform weight 0 for mean
-   float32_t m_weighti;  /// @brief unscented transform weight i for none mean samples
-   float32_t m_kappa{0}; ///< @brief Kappa parameter for finding sigma points
-
-   Vector<DIM_A> m_vecXa{Vector<DIM_A>::Zero()};               /// @brief augmented state vector (incl. process
-                                                               /// and measurement noise means)
-   Matrix<DIM_A, DIM_A> m_matPa{Matrix<DIM_A, DIM_A>::Zero()}; /// @brief augmented state covariance (incl.
-                                                               /// process and measurement noise covariances)
-
-   // Add variables to track the covariances of the process and measurement noise.
-   Matrix<DIM_V, DIM_V> m_matQ; // @brief Process noise covariance matrix
-   Matrix<DIM_N, DIM_N> m_matR; // @brief Measurement noise covariance matrix
-
-public:
    Matrix<DIM_A, SIGMA_DIM_A> m_matSigmaXa{Matrix<DIM_A, SIGMA_DIM_A>::Zero()}; ///< @brief Sigma points matrix
 
-   TrackFitterUKFBase()
-      : KalmanFilter<DIM_X, DIM_Z>(), m_kappa(3 - DIM_A), m_matQ(Matrix<DIM_V, DIM_V>::Zero()),
-        m_matR(Matrix<DIM_N, DIM_N>::Zero())
-   {
-      // 1. calculate weights
-      updateWeights();
-   }
+protected:
+   Vector<DIM_X> m_vecX{Vector<DIM_X>::Zero()};               /// @brief estimated state vector
+   Matrix<DIM_X, DIM_X> m_matP{Matrix<DIM_X, DIM_X>::Zero()}; /// @brief state covariance matrix
 
-   ~TrackFitterUKFBase() {}
+   /// @brief Augmented state vector (incl. process and measurement noise means)
+   Vector<DIM_A> m_vecXa{Vector<DIM_A>::Zero()};
+   /// @brief augmented state covariance (incl. process and measurement noise covariances)
+   Matrix<DIM_A, DIM_A> m_matPa{Matrix<DIM_A, DIM_A>::Zero()};
+
+   // Process and measurement noise covariance matrices
+   /// Process noise covariance matrix (Q)
+   Matrix<DIM_V, DIM_V> m_matQ{Matrix<DIM_V, DIM_V>::Zero()};
+   /// Measurement noise covariance matrix (R)
+   Matrix<DIM_N, DIM_N> m_matR{Matrix<DIM_N, DIM_N>::Zero()};
+
+   /// Unscented transform weight for the mean sigma point
+   float32_t m_weight0;
+   /// Unscented transform weight for the other sigma points
+   float32_t m_weighti;
+   /// Kappa parameter for sigma point calculation
+   float32_t m_kappa{3 - DIM_A};
+
+public:
+   TrackFitterUKFBase() { updateWeights(); }
+   ~TrackFitterUKFBase() = default;
 
    void setKappa(float32_t kappa)
    {
@@ -76,96 +81,43 @@ public:
       updateWeights(); // Update the weights based on the new kappa value
    }
 
-   ///
-   /// @brief adding process noise covariance Q to the augmented state covariance
-   /// matPa in the middle element of the diagonal.
-   ///
-   void setCovarianceQ(const Matrix<DIM_V, DIM_V> &matQ)
-   {
-      m_matQ = matQ; // Store the process noise covariance matrix
-   }
+   /**
+    * @brief Set process noise covariance Q to be used in the prediction step.
+    */
+   void setCovarianceQ(const Matrix<DIM_V, DIM_V> &matQ) { m_matQ = matQ; }
+   /**
+    * @brief Set the measurement noise covariance R to be used in the update step.
+    */
+   void setCovarianceR(const Matrix<DIM_N, DIM_N> &matR) { m_matR = matR; }
+   virtual Vector<DIM_X> &vecX() { return m_vecX; }
+   virtual const Vector<DIM_X> &vecX() const { return m_vecX; }
 
-   ///
-   /// @brief set the measurement noise covariance R to be used in the update step
-   ///
-   void setCovarianceR(const Matrix<DIM_N, DIM_N> &matR)
-   {
-      m_matR = matR; // Store the measurement noise covariance matrix
-   }
+   virtual Matrix<DIM_X, DIM_X> &matP() { return m_matP; }
+   virtual const Matrix<DIM_X, DIM_X> &matP() const { return m_matP; }
 
-   /// Add state vector (m_vecX) to the augment state vector (m_vecXa) and also
-   /// add covariance matrix (m_matP) to the augment covariance (m_matPa).
-   void updateAugWithState()
-   {
-      // Copy state vector to augmented state vector
-      for (int32_t i{0}; i < DIM_X; ++i) {
-         m_vecXa[i] = m_vecX[i];
-      }
-
-      // Copy state covariance matrix to augmented covariance matrix
-      for (int32_t i{0}; i < DIM_X; ++i) {
-         for (int32_t j{0}; j < DIM_X; ++j) {
-            m_matPa(i, j) = m_matP(i, j);
-         }
-      }
-   }
-
-   virtual std::array<float32_t, DIM_V> calculateProcessNoiseMean()
-   {
-      // Calculate the expectation value of the process noise using the current value of the state vector m_vecX
-      std::array<float32_t, DIM_V> processNoiseMean{0};
-
-      // TODO: Set the mean energy loss based on the momentum and particle type. Probably best to track stopping power?
-      return processNoiseMean;
-   }
-
-   virtual Matrix<DIM_V, DIM_V> calculateProcessNoiseCovariance()
-   {
-      // Calculate the process noise covariance matrix
-      Matrix<DIM_V, DIM_V> matQ{Matrix<DIM_V, DIM_V>::Zero()};
-      matQ = m_matQ; // Use the stored process noise covariance matrix
-
-      // TODO: Set the process noise covariance for angular straggle and energy loss.
-      return matQ;
-   }
-
-   void updateAugWithProcessNoise()
-   {
-      auto processNoiseMean = calculateProcessNoiseMean();
-      m_matQ = calculateProcessNoiseCovariance();
-
-      // Add the mean process noise to the augmented state vector
-      for (int32_t i{0}; i < DIM_V; ++i) {
-         m_vecXa[DIM_X + i] = processNoiseMean[i];
-      }
-
-      // Add process noise covariance to the augmented covariance matrix
-      const int32_t S_IDX{DIM_X};
-      const int32_t L_IDX{S_IDX + DIM_V};
-
-      for (int32_t i{S_IDX}; i < L_IDX; ++i) {
-         for (int32_t j{S_IDX}; j < L_IDX; ++j) {
-            m_matPa(i, j) = m_matQ(i - S_IDX, j - S_IDX);
-         }
-      }
-   }
-
-   ///
-   /// @brief update the augmented state vector and covariance matrix
-   /// This functions fully updates the augmented state vector (m_vecXa) and covariance matrix (m_matPa)
-   /// by setting both the state vector and process noise components.
-   ///
+   /**
+    * @brief update the augmented state vector and covariance matrix
+    *
+    * This function fully updates the augmented state vector and covariance matrix using
+    * the state and process noise.
+    */
    void updateAugmentedStateAndCovariance()
    {
       updateAugWithState();
       updateAugWithProcessNoise();
    }
 
-   ///
-   /// @brief state prediction step of the unscented Kalman filter (UKF).
-   /// @param predictionModelFunc callback to the prediction/process model
-   /// function
-   ///
+   /**
+    * @brief state prediction step of the unscented Kalman filter (UKF).
+    * @param predictionModelFunc callback to the prediction/process model function.
+    * @param vecZ actual measurement vector.`
+    *
+    * A template is used here for performace reasons since there is only really a single prediction
+    * model used. Honestly it probably does not make a difference compared to an std::function, but
+    * this was not changed from OpenKF.
+    *
+    * This modifies the state vector and state covariance.
+    */
    template <typename PredictionModelCallback>
    void predictUKF(PredictionModelCallback predictionModelFunc, const Vector<DIM_Z> &vecZ)
    {
@@ -199,11 +151,11 @@ public:
       calculateWeightedMeanAndCovariance<DIM_X>(sigmaXx, m_vecX, m_matP);
    }
 
-   ///
-   /// @brief measurement correction step of the unscented Kalman filter (UKF).
-   /// @param measurementModelFunc callback to the measurement model function
-   /// @param vecZ actual measurement vector.
-   ///
+   /**
+    * @brief measurement correction step of the unscented Kalman filter (UKF).
+    * @param measurementModelFunc callback to the measurement model function
+    * @param vecZ actual measurement vector.
+    */
    template <typename MeasurementModelCallback>
    void correctUKF(MeasurementModelCallback measurementModelFunc, const Vector<DIM_Z> &vecZ)
    {
@@ -235,7 +187,6 @@ public:
       // Add in the measurement noise covariance matrix to the measurement covariance matrix.
       matPzz += m_matR; // Add measurement noise covariance
 
-      // TODO: calculate cross correlation
       const Matrix<DIM_X, DIM_Z> matPxz{calculateCrossCorrelation(sigmaXx, m_vecX, sigmaZ, vecZhat)};
 
       // kalman gain
@@ -246,9 +197,9 @@ public:
    }
 
 protected:
-   ///
-   /// @brief algorithm to calculate the weights used to draw the sigma points
-   ///
+   /**
+    * @brief Set the weights used to calculate sigma points.
+    */
    void updateWeights()
    {
       static_assert(DIM_A > 0, "DIM_A is Zero which leads to numerical issue.");
@@ -258,17 +209,59 @@ protected:
       m_weight0 = m_kappa / denoTerm;
       m_weighti = 0.5F / denoTerm;
    }
+   /**
+    * @brief Add state vector and state covariance matrix to the augmented state vector covariance  matrix.
+    */
+   void updateAugWithState()
+   {
+      // Copy state vector to augmented state vector
+      for (int32_t i{0}; i < DIM_X; ++i) {
+         m_vecXa[i] = m_vecX[i];
+      }
 
-   ///
-   /// @brief algorithm to calculate the deterministic sigma points for
-   /// the unscented transformation
-   ///
-   /// @param vecX mean of the normally distributed state
-   /// @param matPxx covariance of the normally distributed state
-   /// @param STATE_DIM dimension of the vector used to calculate the sigma points
-   /// @param SIGMA_DIM number of sigma points required (default is 2 * STATE_DIM + 1)
-   /// @return matrix of sigma points where each column is a sigma point
-   ///
+      // Copy state covariance matrix to augmented covariance matrix
+      for (int32_t i{0}; i < DIM_X; ++i) {
+         for (int32_t j{0}; j < DIM_X; ++j) {
+            m_matPa(i, j) = m_matP(i, j);
+         }
+      }
+   }
+
+   virtual std::array<float32_t, DIM_V> calculateProcessNoiseMean() { return std::array<float32_t, DIM_V>{0}; }
+
+   virtual Matrix<DIM_V, DIM_V> calculateProcessNoiseCovariance() { return m_matQ; }
+
+   void updateAugWithProcessNoise()
+   {
+      auto processNoiseMean = calculateProcessNoiseMean();
+      m_matQ = calculateProcessNoiseCovariance();
+
+      // Add the mean process noise to the augmented state vector
+      for (int32_t i{0}; i < DIM_V; ++i) {
+         m_vecXa[DIM_X + i] = processNoiseMean[i];
+      }
+
+      // Add process noise covariance to the augmented covariance matrix
+      const int32_t S_IDX{DIM_X};
+      const int32_t L_IDX{S_IDX + DIM_V};
+
+      for (int32_t i{S_IDX}; i < L_IDX; ++i) {
+         for (int32_t j{S_IDX}; j < L_IDX; ++j) {
+            m_matPa(i, j) = m_matQ(i - S_IDX, j - S_IDX);
+         }
+      }
+   }
+
+   /**
+    * @brief Algorithm to calculate the deterministic sigma points for
+    * the unscented transformation.
+    *
+    * @param vecX Mean of the normally distributed state.
+    * @param matPxx Covariance of the normally distributed state.
+    * @param STATE_DIM Dimension of the vector used to calculate the sigma points.
+    * @param SIGMA_DIM Number of sigma points required (default is 2 * STATE_DIM + 1).
+    * @return Matrix of sigma points where each column is a sigma point.
+    */
    template <int32_t STATE_DIM, int32_t SIGMA_DIM = 2 * STATE_DIM + 1>
    Matrix<STATE_DIM, SIGMA_DIM>
    calculateSigmaPoints(const Vector<STATE_DIM> &vecXa, const Matrix<STATE_DIM, STATE_DIM> &matPa)
@@ -321,14 +314,12 @@ protected:
       return sigmaXa;
    }
 
-   ///
-   /// @brief calculate the weighted mean and covariance given a set of sigma
-   /// points
-   /// @param[in] sigmaX matrix of (probably posterior) sigma points where each column contain single
-   /// sigma point.
-   /// @param[out] vecX output weighted mean of the sigma points
-   /// @param[out] matPxx output weighted covariance of the sigma points
-   ///
+   /**
+    * @brief Calculate the weighted mean and covariance given a set of sigma points.
+    * @param[in] sigmaX Matrix of (probably posterior) sigma points where each column contains a single sigma point.
+    * @param[out] vecX Output weighted mean of the sigma points.
+    * @param[out] matPxx Output weighted covariance of the sigma points.
+    */
    template <int32_t STATE_DIM, int32_t SIGMA_DIM>
    void calculateWeightedMeanAndCovariance(const Matrix<STATE_DIM, SIGMA_DIM> &sigmaX, Vector<STATE_DIM> &vecX,
                                            Matrix<STATE_DIM, STATE_DIM> &matPxx)
@@ -356,17 +347,17 @@ protected:
       }
    }
 
-   ///
-   /// @brief calculate the cross-correlation given two sets sigma points X and Y
-   /// and their means x and y
-   /// @param sigmaX first matrix of sigma points where each column contain
-   /// single sigma point
-   /// @param vecX mean of the first set of sigma points
-   /// @param sigmaY second matrix of sigma points where each column contain
-   /// single sigma point
-   /// @param vecY mean of the second set of sigma points
-   /// @return matPxy, the cross-correlation matrix
-   ///
+   /**
+    * @brief calculate the cross-correlation given two sets sigma points X and Y
+    * and their means x and y
+    * @param sigmaX first matrix of sigma points where each column contain
+    * single sigma point
+    * @param vecX mean of the first set of sigma points
+    * @param sigmaY second matrix of sigma points where each column contain
+    * single sigma point
+    * @param vecY mean of the second set of sigma points
+    * @return matPxy, the cross-correlation matrix
+    */
    template <int32_t SIGMA_DIM>
    Matrix<DIM_X, DIM_Z> calculateCrossCorrelation(const Matrix<DIM_X, SIGMA_DIM> &sigmaX, const Vector<DIM_X> &vecX,
                                                   const Matrix<DIM_Z, SIGMA_DIM> &sigmaY, const Vector<DIM_Z> &vecY)
@@ -389,8 +380,11 @@ protected:
    }
 };
 
-// Define template dimension variables for clarity and reuse
-
+/**
+ * @brief Class for fitting tracks using the Unscented Kalman Filter (UKF) algorithm.
+ *
+ * UKF specialized for fitting tracks. This class is where all of the physics is
+ */
 class TrackFitterUKF : public TrackFitterUKFBase<6, 3, 1, 3> {
 protected:
    static constexpr int32_t TF_DIM_X = 6;
@@ -402,6 +396,7 @@ protected:
    std::unique_ptr<AtTools::AtStepper> fStepper{nullptr};
    AtTools::AtPropagator::StepState fMeanStep; /// Holds the step information for POCA propagation of mean state
    ROOT::Math::Plane3D fMeasurementPlane;      ///< Holds the measurement plane for the track fitter
+
 public:
    bool fEnableEnStraggling{true};       ///< @brief Flag to enable/disable energy straggling
    double fMaxStragglingFactor{1. / 3.}; ///< @brief Maximum straggling factor for energy loss
